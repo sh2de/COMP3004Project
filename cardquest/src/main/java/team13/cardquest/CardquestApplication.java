8547b42ee03bbe3778bda84c1814ef0c6a2faee5
sh2de <sh2de@protonmail.com>
2022-02-28 17:48:49 -0500

    spring boot initial

---
+++ cardquest/src/main/java/team13/cardquest/CardquestApplication.java
@@ -0,0 +1,13 @@
+package team13.cardquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardquestApplication.class, args);
+	}
+
+}
---
+++ cardquest/src/test/java/team13/cardquest/CardquestApplicationTests.java
@@ -0,0 +1,13 @@
+package team13.cardquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
