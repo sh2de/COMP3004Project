88f94585c3f32fc389685e18397ff1296cc74a96
sh2de <sh2de@protonmail.com>
2022-03-02 16:47:18 -0500

    hello world succesful!

--- springcardquest/src/main/java/team13/cardquest/CardquestApplication.java
+++ springcardquest/src/main/java/team13/cardquest/CardquestApplication.java
@@ -3,16 +3,26 @@ package team13.cardquest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 //adding for testing purposes
+
+import org.springframework.web.bind.annotation.RestController;
 
 //end
 
 //@SpringBootApplication (original code)
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class CardquestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardquestApplication.class, args);
 	}
 
+	
+	@GetMapping
+	public String hello() {
+    	return "Hello World";
+	}
+	
 }
