3aa28c693e2ce8f8ddd437817cac0bbf9efa4b3f
sh2de <sh2de@protonmail.com>
2022-03-02 15:58:03 -0500

    added mysql to dependencies

--- springcardquest/src/main/java/team13/cardquest/CardquestApplication.java
+++ springcardquest/src/main/java/team13/cardquest/CardquestApplication.java
@@ -2,8 +2,13 @@ package team13.cardquest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//adding for testing purposes
 
-@SpringBootApplication
+//end
+
+//@SpringBootApplication (original code)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CardquestApplication {
 
 	public static void main(String[] args) {
