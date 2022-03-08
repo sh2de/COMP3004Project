9eb7deedb770bc677ec5c2c1f6c7e166a31d7be9
sh2de <sh2de@protonmail.com>
2022-03-07 19:44:22 -0500

    a

--- springcardquest/src/main/java/GameHandler.java
+++ springcardquest/src/main/java/GameHandler.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class GameHandler {
-    public Deck storydeck = new Deck();
+    Deck storydeck = new Deck();
+    int numPlayers = 0;
+    int currentTurn = 1;
+    ArrayList<Player> players = new ArrayList<>();
+
+
+    public void addPlayer(Player p){
+        players.add(p);
+        numPlayers += 1;
+    }
+
+    public GameHandler(){
+
+    }
 }
