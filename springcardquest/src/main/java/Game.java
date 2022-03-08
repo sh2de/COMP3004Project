e3da83f2708628e5e80539a6fb21d4c6743690e2
sh2de <sh2de@protonmail.com>
2022-03-07 22:36:39 -0500

    separated game handler from game state

---
+++ springcardquest/src/main/java/Game.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+//The game object represents the internal state of a game session
+//It is responsible for keeping track of all the game elements, but does not automatically advance the game itself
+public class Game {
+    Deck storydeck = new Deck();
+    int numPlayers = 0;
+    int currentTurn = 1;
+    ArrayList<Player> players = new ArrayList<>();
+
+
+    public void addPlayer(Player p){
+        players.add(p);
+        numPlayers += 1;
+    }
+
+    public void nextTurn(){
+        currentTurn += 1;
+        if (currentTurn > numPlayers){currentTurn = 1;}
+    }
+
+    public Game(){
+
+    }
+}
--- springcardquest/src/main/java/GameHandler.java
+++ springcardquest/src/main/java/GameHandler.java
@@ -1,16 +1,10 @@
 import java.util.ArrayList;
 
 public class GameHandler {
-    Deck storydeck = new Deck();
-    int numPlayers = 0;
-    int currentTurn = 1;
-    ArrayList<Player> players = new ArrayList<>();
+    Game game = new Game();
 
 
-    public void addPlayer(Player p){
-        players.add(p);
-        numPlayers += 1;
-    }
+    
 
     public GameHandler(){
 
