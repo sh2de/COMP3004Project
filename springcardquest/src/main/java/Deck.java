3b7f4255949d70e1312814db16981a5dfd979adc
sh2de <sh2de@protonmail.com>
2022-03-07 22:36:00 -0500

    created basic deck object that draws random cards from its contents

--- springcardquest/src/main/java/Deck.java
+++ springcardquest/src/main/java/Deck.java
@@ -1,9 +1,24 @@
 import java.util.ArrayList;
 
+
 public class Deck {
     public ArrayList<Card> stack = new ArrayList<Card>();
 
+
+    public Card draw(){
+        //removes and returns a random card from the deck
+        if (stack.size() > 0) {
+            int x = (int) (Math.random() * stack.size());
+
+            return stack.remove(x);
+        } else {
+            System.out.println("ERROR: NO CARDS LEFT!");
+            return new StoryCard();
+        }
+
+    }
+
     public Deck(){
-        for (int i = 0; i<100; i++){stack.add(new Card());}
+        for (int i = 0; i<100; i++){stack.add(new StoryCard());}
     }
 }
