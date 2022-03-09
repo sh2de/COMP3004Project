f8a9e43e0f985695e796c146a5fe48523886b5fe
sh2de <sh2de@protonmail.com>
2022-03-09 17:54:03 -0500

    added quest constructor

--- springcardquest/src/main/java/QuestCard.java
+++ springcardquest/src/main/java/QuestCard.java
@@ -1,5 +1,15 @@
 public class QuestCard extends StoryCard{
     String name = "DUMMYQUEST";
     int stages = 3;
-    String foe = "DUMMYFOE";
+    String bossname = "DUMMYFOE";
+    long bossid = -1;
+
+    public QuestCard(long _id, String _url, String _name, int _stages, String _bossname, long _bossid){
+        id = _id;
+        url = _url;
+        name = _name;
+        stages = _stages;
+        bossname = _bossname;
+        bossid = _bossid;
+    }
 }
