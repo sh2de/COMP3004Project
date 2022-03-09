c2e5a05567b8e5d3e0c9f2a19a0cd7eecebca1ad
sh2de <sh2de@protonmail.com>
2022-03-09 17:49:26 -0500

    added constructor for foecard

--- springcardquest/src/main/java/FoeCard.java
+++ springcardquest/src/main/java/FoeCard.java
@@ -2,4 +2,14 @@ public class FoeCard extends AdventureCard{
     String name = "DUMMY FOE";
     int power = 20;
     int bosspower = 30;
+    boolean mordred = false;
+
+    public FoeCard(long _id, String _url, String _name, int _power, int _bosspower, boolean _mordred){
+        id = _id;
+        url = _url;
+        name = _name;
+        power = _power;
+        bosspower = _bosspower;
+        mordred = _mordred;
+    }
 }
