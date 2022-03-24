0778bf3bfd716d07ef85f507724e86b657b72bea
Sami Alasfahani <57428851+SamiAlasfahani@users.noreply.github.com>
2022-03-23 20:04:44 -0400

    Update Card.java

--- springcardquest/src/main/java/team13/cardquest/Card.java
+++ springcardquest/src/main/java/team13/cardquest/Card.java
@@ -1,50 +1,5 @@
 package team13.cardquest;
 
-public abstract class Card {
-    protected long id = -1;
-    protected String name = "UNNAMED";
-    protected String type = "UNDEFINED";
-    protected int bidWorth = 1;
-    protected String url = "./images/testcard.png";
-    protected int frequency=0;
+public class Card {
 
-    public boolean initQuest(Quest q){
-        return false;
-    }
-
-    public String toString(){
-        return "GENERIC CARD :( ";
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public String getType(){
-        return type;
-    }
-
-    public int getBidWorth(){
-        return bidWorth;
-    }
-
-    public int getFrequency(){
-        return frequency;
-    }
-
-    public String getUrl(){
-        return url;
-    }
-
-
-    //doesn't make sense for Card to have, should be for specific type of
-    public int getPower(){
-        return 0;
-    }
-	
-	public abstract void play();
 }
