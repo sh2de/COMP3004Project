1550b4bf71a40d40bf47fb7a733c24f8689b1a97
sh2de <sh2de@protonmail.com>
2022-03-25 16:25:53 -0400

    a

--- springcardquest/src/main/java/team13/cardquest/gameTest.java
+++ springcardquest/src/main/java/team13/cardquest/gameTest.java
@@ -8,16 +8,30 @@ public class gameTest {
         System.out.println("testing the game");
 
         Game game = new Game(); //create new game
-        Scanner getInput = new Scanner(System.in);
-        System.out.println("Enter player name");
-        String name = (getInput.nextLine());  // Read user input
-        game.joinGame(name);
+        //Scanner getInput = new Scanner(System.in);
+        //System.out.println("Enter player name");
+        //String name = (getInput.nextLine());  // Read user input
+        //game.joinGame(name);
 
-        while(true){
+        /*while(true){
             executeSignals(game.getUpdates(name), name, game);
-        }
+        }*/
+
+        //WRITE TEST CODE HERE----------------------------------
+        String p1 = "player 1";
+        String p2 = "player 2";
+        game.joinGame(p1);
+        game.joinGame(p2);
+        game.startGame(p1);
+        game.startGame(p2);
+        game.print();
+        
+        
+        
+        //TEST CODE END-----------------------------------------
     }
 
+    /*
     public static void executeSignals(ArrayList<String> commands, String name, Game game){
         for (String signal : commands) {
             switch(signal){
@@ -33,7 +47,7 @@ public class gameTest {
                     break;
             }
         }
-    }
+    }*/
  
 }
 /*
