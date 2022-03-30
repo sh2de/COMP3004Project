5c21bb92833c8f673605734df3cc1ce1bd392bb1
sh2de <sh2de@protonmail.com>
2022-03-30 02:34:43 -0400

    added discard functionality and deck reshuffling

--- springcardquest/src/main/java/team13/cardquest/Deck.java
+++ springcardquest/src/main/java/team13/cardquest/Deck.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Deck {
 
     private ArrayList<Card> cards;
+    private ArrayList<Card> discards;
 
     public Deck(){
         cards = new ArrayList<>();
@@ -19,13 +20,21 @@ public class Deck {
         }
     }
 
-    public Card draw(){
-        //removes and returns a random card from the deck
-        //if (stack.size() == 0){initialize();}
+    public Card draw(){//removes and returns a random card from the deck
+        
+        //before drawing, refresh the deck if nessecary
+        if (cards.size() == 0){
+            cards = discards;
+            discards = new ArrayList<>();
+        }
         
         int x = (int) (Math.random() * cards.size());
 
         return cards.remove(x);
+    }
+
+    public void discard(Card c){
+        discards.add(c);
     }
 
 }
