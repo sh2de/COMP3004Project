7cfa368ad9a7fa2441042be80f544cbcf0d57e0b
Sami Alasfahani <dindle21@gmail.com>
2022-04-06 14:45:02 -0400

    added url to allyblob

--- springcardquest/src/main/java/team13/cardquest/BlobAlly.java
+++ springcardquest/src/main/java/team13/cardquest/BlobAlly.java
@@ -4,10 +4,12 @@ public class BlobAlly {
     String name;
 	int power;
 	int value;
+	String url;
 
-	public BlobAlly(String name, int power, int value){
+	public BlobAlly(String name, int power, int value, String url){
 		this.name = name;
 		this.power = power;
 		this.value = value;
+		this.url = url;
 	}
 }
